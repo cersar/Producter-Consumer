8d26d2fea70e102d13579a84818ad9cb621f65f0
cersar <1319839558@qq.com>
2018-06-17 22:44:51 +0800

    Create buffer.h

---
+++ buffer.h
@@ -0,0 +1,14 @@
+/*
+ * buffer.h
+ *
+ *  Created on: Jun 17, 2018
+ *      Author: yangsen
+ */
+
+#ifndef BUFFER_H_
+#define BUFFER_H_
+
+typedef int buffer_item;
+#define BUFFER_SIZE 5
+
+#endif /* BUFFER_H_ */
